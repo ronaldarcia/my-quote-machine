87475f972aaad585d77a1e3222170842145b553f
Ronald Arcia <ronald.arcia.ar@gmail.com>
2023-11-13 11:07:00 -0300

    my first quote machine commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { FaInstagram, FaFacebookF } from 'react-icons/fa';
+import './myCss.css';
 
 function App() {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  
+  const fetchQuote = async () => {
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    setQuote(data.content);
+    setAuthor(data.author);
+    
+};
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+
+      <div className="quoteBox">
+
+          <p className='quote'>"{quote}"</p>
+          <p className='author'>- {author} -</p>
+
+      <div className='social'>
+          
+          <a className='link1' href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <FaFacebookF/>
+          </a>
+          <a className='link2' href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <FaInstagram/>
+          </a>
+          
+          <button className='button' onClick={fetchQuote}>New Quote</button>
+          
+      </div>
+            
+      </div>
+      
+      <div className='footer'>
+          <p>© 2023 by rodadev</p>
+      </div>
     </div>
   );
 }
